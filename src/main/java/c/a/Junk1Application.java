605dcd2f6c090dff17129733163119d633325324
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 00:45:48 +0000

    Initial commit from Atomist

---
+++ src/main/java/c/a/Junk1Application.java
@@ -0,0 +1,12 @@
+package c.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junk1Application.class, args);
+	}
+}
